3c47ee5f580856361c54567e3b7155ef70ba5365
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2024-03-07 00:15:27 +0530

    Cleanup

--- fastboot/__init__.py
+++ fastboot/__init__.py
@@ -1,3 +1,19 @@
-from fastboot.utils.version import read_version
+import importlib.metadata
+import tomllib
 
-__version__ = read_version()
+from fastboot.constants import PROJECT_DIR
+
+__all__ = ["__version__", "__author__"]
+
+# Project Information
+__author__ = ["Arpan Mahanty <arpan.mahanty.007@gmail.com>"]
+__license__ = "MIT"
+
+try:
+    # Read version from pyproject file during development
+    with open(PROJECT_DIR / "pyproject.toml", mode="rb") as pyproject_file:
+        __version__: str = tomllib.load(pyproject_file)["tool"]["poetry"]["version"]
+
+except FileNotFoundError:
+    # Read from the package metadata
+    __version__: str = importlib.metadata.version(__package__ or __name__.split(".", maxsplit=1)[0])
---
+++ fastboot/constants.py
@@ -0,0 +1,3 @@
+import pathlib
+
+PROJECT_DIR: pathlib.Path = pathlib.Path(__file__).parents[1]
