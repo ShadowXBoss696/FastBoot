0de9a3f79c9976b3aedd763edb8a3668a2333498
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2024-05-19 14:26:23 +0530

    Updated the mechanism to retrieve the version according to the installation type

--- fastboot/__main__.py
+++ fastboot/__main__.py
@@ -1,3 +1,14 @@
+#  Copyright (c) 2024 Arpan Mahanty
+#
+#  This file is part of FastBoot released under the MIT license.
+#  See the LICENSE for more information.
+#
+#  This file is part of FastBoot released under the MIT license.
+#  See the LICENSE for more information.
+#
+#  This file is part of FastBoot released under the MIT license.
+#  See the LICENSE for more information.
+
 import fastboot
 
 if __name__ == "__main__":
---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+#  Copyright (c) 2024 Arpan Mahanty
+#
+#  This file is part of FastBoot released under the MIT license.
+#  See the LICENSE for more information.
+#
+#  This file is part of FastBoot released under the MIT license.
+#  See the LICENSE for more information.
+#
+#  This file is part of FastBoot released under the MIT license.
+#  See the LICENSE for more information.
